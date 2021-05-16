e66d7facd56de0c96b3347a15c5210e678aee573
Amanda Kirchner <geeky.brewster@gmail.com>
2021-05-16 14:41:12 -0500

    Add componentDidMount method and some refactoring

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom';
 //import SeasonDisplay from './components/SeasonDisplay'
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
 
-    //initialize state obj
-    //This is the ONLY time we do a direct assignment to this.state!!
-    this.state = { latitude: null, longitude: null };
+  // constructor(props){
+  //   super(props);
 
+  //   //initialize state obj -- This is the ONLY time we do a direct assignment to this.state!!
+  //   this.state = { latitude: null, longitude: null, errorMessage: ''};
+  // };
+
+  //Equivalent to the constructor setup above, which is only initializing state
+  state = { latitude: null, longitude: null, errorMessage: ''};
+
+  componentDidMount() {
+    //called once automatically after component is added to DOM
+    //good place to do initial data loading
     window.navigator.geolocation.getCurrentPosition(
       position => {
         //setState is always used to change state's value!!
@@ -19,16 +26,21 @@ class App extends React.Component {
           errorMessage: ''
         }); 
       },
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      err => this.setState({ errorMessage: err.message })
     );
-  };
+  }
+  
+  componentDidUpdate(){
+    //called automatically after component is updated
+    //good place to do more data loading when state/props change
+    console.log('Component was updated and re-rendered');
+  }
 
   //Render gets called all the dang time, so don't add requests, calls, etc.
+  //Ideally, render should only be used to return some JSX
   render() {
       if (this.state.errorMessage && (!this.state.latitude && !this.state.longitude)){
-        return <div>{this.state.errorMessage}</div>;
+        return <div>Error: {this.state.errorMessage}</div>;
       }
 
       if (!this.state.errorMessage && (this.state.latitude && this.state.longitude)){
