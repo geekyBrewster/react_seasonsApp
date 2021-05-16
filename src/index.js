69f081ab7bae89be71e82901a344d5fe311254a0
Amanda Kirchner <geeky.brewster@gmail.com>
2021-05-16 11:57:05 -0500

    Added state to component getting lat/long

--- src/index.js
+++ src/index.js
@@ -3,13 +3,33 @@ import ReactDOM from 'react-dom';
 //import SeasonDisplay from './components/SeasonDisplay'
 
 class App extends React.Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {console.log(position)},
-      (err) => {console.log(err)}
-    );
+  constructor(props){
+    super(props);
 
-    return <div>Latitude: </div>;
+    //initialize state obj
+    //This is the ONLY time we do a direct assignment to this.state!!
+    this.state = { latitude: null, longitude: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        //setState is always used to change state's value!!
+        this.setState({ 
+          latitude: position.coords.latitude, 
+          longitude: position.coords.longitude 
+        }); 
+      },
+      err => {console.log(err)}
+    );
+  };
+
+  //Render gets called all the dang time, so don't add requests, calls, etc.
+  render() {
+    return (
+      <div>
+        <div>Latitude: {this.state.latitude}</div>
+        <div>Longitude: {this.state.longitude}</div>
+      </div>
+    );
   }
 }
 
